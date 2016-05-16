2dd18bb987686e825a89b666754f55965495c90e
Vlad Frolov <frolvlad@gmail.com>
2016-05-16 09:27:02 +0300

    Pinned CMAKE_INSTALL_LIBDIR to 'lib', so it doesn't build into 'lib64' (#2)
    * Pinned CMAKE_INSTALL_LIBDIR to 'lib', so it doesn't build into 'lib64'
    
    * Fixed Windows bld.bat due to the changes in conda-build 1.20.2 related to setting environment variables without quotes
    
    [skip ci]

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -4,10 +4,11 @@ cd build
 :: CAPNP_LITE=ON is required since Cap'n Proto doesn't have complete support on MSVC:
 :: https://github.com/sandstorm-io/capnproto/issues/227
 cmake ^
-    -G%CMAKE_GENERATOR% ^
+    -G"%CMAKE_GENERATOR%" ^
     -DCAPNP_LITE=ON ^
     -DBUILD_TESTING=OFF ^
-    -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+    -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
+    -DCMAKE_INSTALL_LIBDIR=lib ^
     ..\c++
 if errorlevel 1 exit 1
 
