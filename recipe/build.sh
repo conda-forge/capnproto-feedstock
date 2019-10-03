550fb23001f419b4a8ac819ac95b732b5106415f
Isaiah Norton <isaiah@tiledb.io>
2019-10-03 18:55:12 +0000

    Clean up build.sh, fix mac build
    CMake does not correctly detect the Anaconda compilers, because it
    expects the compiler name 'AppleClang' on Apple. We know we have a
    C++14 compiler, so the simplest fix is to tell CMake that the
    compiler has the feature capnproto wants, by invoking with
    
      -DCMAKE_CXX_COMPILE_FEATURES=cxx_constexpr

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,28 +1,29 @@
 #!/bin/bash
 
+set -x
+
 mkdir build
 cd build
 
-CMAKE_CXX_FLAGS="-fPIC"
-if [ "$(uname)" = "Darwin" ]; then
+declare -a CMAKE_PLATFORM_FLAGS
+if [[ ${HOST} =~ .*darwin.* ]]; then
+    CMAKE_PLATFORM_FLAGS+=(-DCMAKE_OSX_SYSROOT="${CONDA_BUILD_SYSROOT}")
     # "-stdlib=libc++ -mmacosx-version-min=10.7" are required to enable C++11 features
-    CMAKE_CXX_FLAGS="$CMAKE_CXX_FLAGS -stdlib=libc++ -mmacosx-version-min=10.7"
-    EXTRA_CMAKE_ARGS="-DCMAKE_OSX_DEPLOYMENT_TARGET=10.7"
+    #CMAKE_CXX_FLAGS="$CMAKE_CXX_FLAGS -stdlib=libc++ -mmacosx-version-min=10.7"
+    #EXTRA_CMAKE_ARGS="-DCMAKE_OSX_DEPLOYMENT_TARGET=10.7"
     # Disable testing on OS X due to CMake config bugs fixed only in master:
     # https://github.com/sandstorm-io/capnproto/issues/322
-    EXTRA_CMAKE_ARGS="$EXTRA_CMAKE_ARGS -DBUILD_TESTING=OFF"
+    EXTRA_CMAKE_ARGS="$EXTRA_CMAKE_ARGS -DBUILD_TESTING=OFF -DCMAKE_CXX_COMPILE_FEATURES=cxx_constexpr"
 fi
 
 cmake \
     -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_CXX_FLAGS="$CMAKE_CXX_FLAGS" \
-    -DCMAKE_CXX_LINK_FLAGS="$CMAKE_CXX_LINK_FLAGS" \
     -DCMAKE_INSTALL_PREFIX="$PREFIX" \
     -DCMAKE_INSTALL_LIBDIR="lib" \
     $EXTRA_CMAKE_ARGS \
     ../c++
 
-cmake --build .
+cmake --build . -- -j ${CPU_COUNT}
 
 if [ "$(uname)" != "Darwin" ]; then
     cmake --build . --target check
