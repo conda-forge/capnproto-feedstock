627e973abfdf8761e48c05fad3b87737f27f7d52
mlehurau <max.lehuraux@analog.com>
2021-12-17 14:02:11 -0500

    Specify NMake as cmake generator on Windows

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,6 +2,7 @@ mkdir build
 cd build
 
 cmake ^
+    -G "NMake Makefiles" ^
     -DCMAKE_BUILD_TYPE=Release ^
     -DCMAKE_POSITION_INDEPENDENT_CODE=1 ^
     -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
