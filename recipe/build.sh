d3a7e5190aeb74df7f1ba8353ccf3bd18d7334a2
Wolf Vollprecht <w.vollprecht@gmail.com>
2022-01-19 11:13:52 +0100

    remove some files, use autoconf

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,28 +2,37 @@
 
 set -x
 
-mkdir build
-cd build
+autoreconf -vfi
+./configure \
+    --enable-shared \
+    --prefix=$PREFIX
 
-declare -a CMAKE_PLATFORM_FLAGS
-if [[ ${HOST} =~ .*darwin.* ]]; then
-    CMAKE_PLATFORM_FLAGS+=(-DCMAKE_OSX_SYSROOT="${CONDA_BUILD_SYSROOT}")
-fi
+make -j${CPU_COUNT}
+make install
 
-if [ "$(uname)" != "Darwin" ]; then
-    EXTRA_CMAKE_ARGS="$EXTRA_CMAKE_ARGS -DCMAKE_CXX_FLAGS=\"-lrt\""
-fi
+# mkdir build
+# cd build
 
-cmake \
-    -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_POSITION_INDEPENDENT_CODE=1 \
-    -DCMAKE_INSTALL_PREFIX="$PREFIX" \
-    -DCMAKE_INSTALL_LIBDIR="lib" \
-    $EXTRA_CMAKE_ARGS \
-    ..
+# declare -a CMAKE_PLATFORM_FLAGS
+# if [[ ${HOST} =~ .*darwin.* ]]; then
+#     CMAKE_PLATFORM_FLAGS+=(-DCMAKE_OSX_SYSROOT="${CONDA_BUILD_SYSROOT}")
+# fi
 
-cmake --build . --parallel ${CPU_COUNT}
+# if [ "$(uname)" != "Darwin" ]; then
+#     EXTRA_CMAKE_ARGS="$EXTRA_CMAKE_ARGS -DCMAKE_CXX_FLAGS=\"-lrt\""
+# fi
 
-cmake --build . --target check
+# cmake \
+#     -DCMAKE_BUILD_TYPE=Release \
+#     -DCMAKE_POSITION_INDEPENDENT_CODE=1 \
+#     -DCMAKE_INSTALL_PREFIX="$PREFIX" \
+#     -DCMAKE_INSTALL_LIBDIR="lib" \
+#     -DBUILD_SHARED=ON \
+#     $EXTRA_CMAKE_ARGS \
+#     ..
 
-cmake --build . --target install
+# cmake --build . --parallel ${CPU_COUNT}
+
+# cmake --build . --target check
+
+# cmake --build . --target install
