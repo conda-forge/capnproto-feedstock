c60c5ca17aa48e4d8b7de6786a2f9341e4c01986
Kai Tietz <ktietz@anaconda.com>
2022-02-03 13:38:52 +0100

    move config change before do reconfig in subdir

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,15 +2,16 @@
 
 set -x
 
-cd c++
 if [[ $target_platform == osx-* ]]; then
     list_config_to_patch=$(find . -name config.guess | sed -E 's/config.guess//')
     for config_folder in $list_config_to_patch; do
         echo "copying config to $config_folder ...\n"
         cp -v $BUILD_PREFIX/share/libtool/build-aux/config.* $config_folder
     done
-    autoreconf -vfi
+
 fi
+cd c++
+autoreconf -vfi
 
 if [[ $target_platform == "linux-"* ]]; then
     export LIBS="-lrt $LIBS"
